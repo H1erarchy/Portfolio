6895f394d890be10ca8ee8472cbbba08e6630844
Your Name <teonatskhadaia@yexample.com$ git config --global user.name teona>
2023-03-27 22:33:48 -0400

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 export default function Hero() {
+  const heroStyles = {backgroundColor:"rgb(206,130,255)" , height:"100vh"}
 
-
-const {name, apple} = useParams ()
-console.log(apple)
   return (
-    <div>
+    <div style={heroStyles}>
         <div>
             Teona Tskhadaia
         </div>
         <div>
             Software Developer
         </div>
-        <button>  <a href="https://www.linkedin.com/in/teona-tskhadaia-89473a25a/"> Contact Me!</a></button>
-{apple}
+        
+        <Button variant="outline-warning"><a href="https://www.linkedin.com/in/teona-tskhadaia-89473a25a/"> Contact Me!</a></Button>
+
     </div>
     
     
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
 
 export default function Project() {
+  const myProjects = [{title:"Loan Calculator"}, {title:"Payroll System"}, {title:"Bug Tracker"}]
   return (
-    <div>project</div>
+    <div>
+      <h2>project</h2>
+      {myProjects.map(project =>
+      <ProjectCard/>)}
+      
+      </div>
   )
 }
