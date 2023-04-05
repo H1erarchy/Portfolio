cfe2bff87744bf591e28bf24bfc76d08f107f097
Kp <kpericles23@hotmail.com>
2023-04-04 21:57:18 -0400

    porf

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
 import Contact from './components/Contact'
-import Calculator from './Projects/Calculator';
+//import Calculator from './Projects/Calculator';
 import NotFound from './components/NotFound';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home'
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,18 @@ import { useParams } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
 
 export default function Hero() {
-  const heroStyles = {backgroundColor:"rgb(206,130,255)" , height:"100vh"}
+  const heroStyles = {backgroundColor:"rgb(3, 102, 101)" , height:"100vh"}
 
   return (
     <div style={heroStyles}>
         <div>
-            Teona Tskhadaia
+            Kissny Pierre
         </div>
         <div>
-            Software Developer
+            Junior Software Developer
         </div>
         
-        <Button variant="outline-warning"><a href="https://www.linkedin.com/in/teona-tskhadaia-89473a25a/"> Contact Me!</a></Button>
+        <Button variant="outline-warning"><a href="https://www.linkedin.com/in/kissny-pierre-6062748a/"> Contact Me!</a></Button>
 
     </div>
     
